3a6871e072057e41aac8398047acad4472f2d605
Victor <VictorCarmona9035@hotmail.com>
2023-08-01 20:32:48 +0200

    15-CAPA PRESENTACION: Agregando el formulario principal MDI

--- Sistema2023.Presentacion/Program.cs
+++ Sistema2023.Presentacion/Program.cs
@@ -16,7 +16,7 @@ namespace Sistema2023.Presentacion
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FrmPrincipal());
         }
     }
 }
