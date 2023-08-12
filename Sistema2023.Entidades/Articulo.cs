e32bda1f24c924f7f3986c20ca7164c4499d8a3d
Victor <VictorCarmona9035@hotmail.com>
2023-08-12 12:00:00 +0200

    [FIX] CAPA ENTIDADES: Entidad Artículo

--- Sistema2023.Entidades/Articulo.cs
+++ Sistema2023.Entidades/Articulo.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Sistema2023.Entidades
+﻿namespace Sistema2023.Entidades
 {
-    internal class Articulo
+    public class Articulo
     {
+        public int IdArticulo { get; set; }
+        public int IdCategoria { get; set; }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public decimal PrecioVenta { get; set; }
+        public int Stock { get; set; }
+        public string Descripcion { get; set; }
+        public string Imagen { get; set; }
+        public bool Estado { get; set; }
     }
 }
